cf4535f1f581be666c1a7f6eada62081cafac129
hiramgao <hiramgai@163.com>
2021-10-23 14:44:02 +0800

    siderbar toggle

--- themes/QAQ/source/js/main.js
+++ themes/QAQ/source/js/main.js
@@ -35,5 +35,48 @@ $(function () {
 
     $(window).scroll(winScroll);
 
+    const toggle = $('#sidebar-toggle')
+    const sidebar = $('#sidebar')
+    const context = $('#context')
+
+    let isHide = false
+    let isAnimating = false
+
+    function closeAnimating() {
+        isAnimating = false
+    }
+
+    function hideContext() {
+        $('.sidebar-main').hide(500)
+        $('.copyright').hide(500)
+        sidebar.animate({
+            width: "24px"
+        }, 500);
+        context.animate({
+            width: `${$('.max-width-4').width() - 24}px`,
+            paddingLeft: 0
+        }, 1000, "linear", closeAnimating);
+    }
+    function showContext() {
+        console.log('showContext')
+        $('.sidebar-main').show(500)
+        $('.copyright').show(500)
+        sidebar.animate({
+            width: "25%"
+        }, 1000, 'linear', closeAnimating);
+        context.animate({
+            width: "75%",
+            paddingLeft: '1rem'
+        }, 500);
+    }
+
+    toggle.on('click', function () {
+        if (!isAnimating) {
+            isAnimating = true
+            isHide ? showContext() : hideContext()
+            isHide = !isHide
+        }
+    })
+
 })
 
